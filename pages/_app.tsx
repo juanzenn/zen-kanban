77cfe23832b7600e6611ee22f85d8171ea661227
Juan Alvarez <juanandres140299@gmail.com>
2021-09-04 13:44:30 -0400

    Preflight (normalize.css) from @tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../styles/reset.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
